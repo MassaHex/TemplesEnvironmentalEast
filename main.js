48585dbe457b234f33133c99c71930190864e1f5
DESKTOP-H2N0PH4\roger <105466578+MassaHex@users.noreply.github.com>
2024-10-14 17:59:15 -0400

    Smoother Theme transition + better themes and nicers switch animations.

--- main.js
+++ main.js
@@ -1,22 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
     const checkbox = document.getElementById('modecheckbox');
-    const lightMode = document.getElementById('light-mode');
-    const darkMode = document.getElementById('dark-mode');
     const toggleContainer = document.querySelector(".toggle-container");
+    const rootElement = document.documentElement;
 
     // Load user's theme preference from local storage
     const currentTheme = localStorage.getItem('theme');
     if (currentTheme) {
         if (currentTheme === 'dark') {
-            darkMode.disabled = false;
-            lightMode.disabled = true;
             checkbox.checked = true;  // Set the checkbox to dark mode
             toggleContainer.classList.add("checked");  // Add visual changes for dark mode
+            rootElement.setAttribute('data-theme', 'dark');
         } else {
-            darkMode.disabled = true;
-            lightMode.disabled = false;
             checkbox.checked = false;
             toggleContainer.classList.remove("checked");  // Remove visual changes for light mode
+            rootElement.setAttribute('data-theme', 'light');
         }
     }
 
@@ -24,16 +21,14 @@ document.addEventListener('DOMContentLoaded', function() {
     checkbox.addEventListener('change', function() {
         if (this.checked) {
             // Switch to dark mode
-            darkMode.disabled = false;
-            lightMode.disabled = true;
             localStorage.setItem('theme', 'dark'); // Save the user's preference
             toggleContainer.classList.add("checked");  // Apply visual changes for dark mode
+            rootElement.setAttribute('data-theme', 'dark');
         } else {
             // Switch to light mode
-            darkMode.disabled = true;
-            lightMode.disabled = false;
             localStorage.setItem('theme', 'light'); // Save the user's preference
             toggleContainer.classList.remove("checked");  // Remove visual changes for dark mode
+            rootElement.setAttribute('data-theme', 'light');
         }
     });
 });
